ed5b2436b5e9954065742ffc33948403207a4ed0
Jonathan Sommer <johnyso@web.de>
2014-11-04 16:20:43 +0100

    add packages

---
+++ app/src/main/java/de/ibs/app/room/RoomActivity.java
@@ -0,0 +1,16 @@
+package de.ibs.app.room;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import de.ibs.app.R;
+
+public class RoomActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
---
+++ app/src/main/java/de/ibs/app/room/roomModel.java
@@ -0,0 +1,7 @@
+package de.ibs.app.room;
+
+/**
+ * Created by johnyso on 04.11.14.
+ */
+public class RoomModel {
+}
