733d83432b84b63bc958e18ad1595cf9f37f9c0f
Jonathan Sommer <johnyso@web.de>
2014-11-05 12:14:03 +0100

    change name

--- app/src/main/java/de/ibs/app/room/RoomOverview.java
+++ app/src/main/java/de/ibs/app/room/RoomOverview.java
@@ -1,9 +1,7 @@
 package de.ibs.app.room;
 
 import android.app.Fragment;
-import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,7 +16,7 @@ public class RoomOverview extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_start_screen, container, false);
+        return inflater.inflate(R.layout.room_overview_fragment, container, false);
     }
 
 }
