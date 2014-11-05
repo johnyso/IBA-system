d3984da50abb38ec6902ff9f733ec0a8a4baca68
Jonathan Sommer <johnyso@web.de>
2014-11-05 10:34:19 +0100

    delete not used Files

--- app/src/main/java/de/ibs/app/room/provider/roomProvider.java
+++
@@ -1,7 +0,0 @@
-package de.ibs.app.room.provider;
-
-/**
- * Created by johnyso on 04.11.14.
- */
-public class RoomProvider {
-}
--- app/src/main/java/de/ibs/app/room/roomModel.java
+++
@@ -1,7 +0,0 @@
-package de.ibs.app.room;
-
-/**
- * Created by johnyso on 04.11.14.
- */
-public class RoomModel {
-}
